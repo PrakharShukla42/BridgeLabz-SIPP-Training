90344575ff32fdba34498fdd568d8872bc8a6485
Prakhar Shukla <therockstarop9956@gmail.com>
2025-08-07 20:43:47 +0530

    [Prakhar Shukla] add. BuiltIn Case Conversion added

---
+++ Day4_Strings/BuiltInCaseConversion.java
@@ -0,0 +1,9 @@
+package Day4_Strings;
+
+public class BuiltInCaseConversion {
+    public static void run() {
+        String s = "PrakharShukla";
+        System.out.println("Uppercase (built-in): " + s.toUpperCase());
+        System.out.println("Lowercase (built-in): " + s.toLowerCase());
+    }
+}
