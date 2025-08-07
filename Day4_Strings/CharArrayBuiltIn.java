0859126de7a97fe2cc51f5ed6c51e54fec39cfc8
Prakhar Shukla <therockstarop9956@gmail.com>
2025-08-07 20:44:33 +0530

    [Prakhar Shukla] add. Char Array BuiltIn added

---
+++ Day4_Strings/CharArrayBuiltIn.java
@@ -0,0 +1,11 @@
+package Day4_Strings;
+
+import java.util.Arrays;
+
+public class CharArrayBuiltIn {
+    public static void run() {
+        String s = "Prakhar";
+        char[] chars = s.toCharArray();
+        System.out.println("Char array (built-in): " + Arrays.toString(chars));
+    }
+}
