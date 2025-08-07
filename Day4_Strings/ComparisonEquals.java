d192f8e77f714eb8dfecfbfbabbd5eb27817eaf8
Prakhar Shukla <therockstarop9956@gmail.com>
2025-08-07 20:46:05 +0530

    [Prakhar Shukla] add. Comparison Equals added

---
+++ Day4_Strings/ComparisonEquals.java
@@ -0,0 +1,9 @@
+package Day4_Strings;
+
+public class ComparisonEquals {
+    public static void run() {
+        String a = "BridgeLabz";
+        String b = "BridgeLabz";
+        System.out.println("Are equal using .equals(): " + a.equals(b));
+    }
+}
