4bfc5f332eb691de1c68bfd6fa5bb11083036c72
Prakhar Shukla <therockstarop9956@gmail.com>
2025-08-07 20:47:39 +0530

    [Prakhar Shukla] add. Substring Method added

---
+++ Day4_Strings/SubstringMethod.java
@@ -0,0 +1,8 @@
+package Day4_Strings;
+
+public class SubstringMethod {
+    public static void run() {
+        String s = "ManuSir";
+        System.out.println("Substring using substring(): " + s.substring(2, 5));
+    }
+}
